9012a63bd9d4a1873bfc0f45afab05ea815e6f8e
COHRINT Lab <jamc3951@colorado.edu>
2020-04-11 20:42:42 -0600

    make some script placeholders

---
+++ odefun2.m
@@ -0,0 +1,10 @@
+% Jamison McGinley
+% 2/4/19
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function ddt = odefun2(t,IC)
+
+
+
+ddt = [dy];
+
+end
---
+++ progress1.m
@@ -0,0 +1,31 @@
+clear; close; clc;
+
+% Use ode45 to get out expected results from the Nonlinear System
+
+nom_cond = [];
+perturbation; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
